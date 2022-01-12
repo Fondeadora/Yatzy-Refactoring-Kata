9fb84c61e4b7c3937dd329e61dd20f0d86a7614d
Carlos Correa <carlosx-34@hotmail.com>
2022-01-12 11:51:33 -0600

    fix: chance return d plus and typing args

--- python/yatzy.py
+++ python/yatzy.py
@@ -1,14 +1,8 @@
 class Yatzy:
 
     @staticmethod
-    def chance(d1, d2, d3, d4, d5):
-        total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
-        return total
+    def chance(d1: int, d2: int, d3: int, d4: int, d5: int):
+        return d1 + d2 + d3 + d4 + d5
 
     @staticmethod
     def yatzy(dice):
