0a029b843587a3ea7bf7d4503f6a34402d07dfd3
Carlos Correa <carlosx-34@hotmail.com>
2022-01-12 13:03:23 -0600

    refactor: final

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -18,14 +18,14 @@ def test_yatzy_scores_50():
         assert 0 == Yatzy.yatzy([6,6,6,6,3])
   
 
-def test_1s():
+def test_ones():
         assert Yatzy.ones(1,2,3,4,5) == 1
         assert 2 == Yatzy.ones(1,2,1,4,5)
         assert 0 == Yatzy.ones(6,2,2,4,5)
         assert 4 == Yatzy.ones(1,2,1,1,1)
   
 
-def test_2s():
+def test_twos():
         assert 4 == Yatzy.twos(1,2,3,2,6)
         assert 10 == Yatzy.twos(2,2,2,2,2)
   
@@ -35,7 +35,7 @@ def test_threes():
         assert 12 == Yatzy.threes(2,3,3,3,3)
   
 
-def test_fours_test():
+def test_fours():
         assert 12 == Yatzy.fours(4,4,4,5,5)
         assert 8 == Yatzy.fours(4,4,5,5,5)
         assert 4 == Yatzy.fours(4,5,5,5,5)
@@ -47,7 +47,7 @@ def test_fives():
         assert 20 == Yatzy.fives(4,5,5,5,5)
   
 
-def test_sixes_test():
+def test_sixes():
         assert 0 == Yatzy.sixes(4,4,4,5,5)
         assert 6 == Yatzy.sixes(4,4,6,5,5)
         assert 18 == Yatzy.sixes(6,5,6,6,5)
@@ -59,7 +59,7 @@ def test_one_pair():
         assert 12 == Yatzy.score_pair(5,3,6,6,5)
         
 
-def test_two_Pair():
+def test_two_pair():
         assert 16 == Yatzy.two_pair(3,3,5,4,5)
         assert 18 == Yatzy.two_pair(3,3,6,6,6)
         assert 0 == Yatzy.two_pair(3,3,6,5,4)
@@ -71,7 +71,7 @@ def test_three_of_a_kind():
         assert 9 == Yatzy.three_of_a_kind(3,3,3,3,5)
   
 
-def test_four_of_a_knd():
+def test_four_of_a_kind():
         assert 12 == Yatzy.four_of_a_kind(3,3,3,3,5)
         assert 20 == Yatzy.four_of_a_kind(5,5,5,4,5)
         assert 12 == Yatzy.four_of_a_kind(3,3,3,3,3)
